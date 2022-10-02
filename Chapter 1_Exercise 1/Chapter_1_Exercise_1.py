a46514e4fe36868a90a7cada5380bfbb87469e87
Juliannnnnnn <91421563+Juliannnnnnn@users.noreply.github.com>
2022-10-02 14:49:10 +0400

    Delete Chapter 1_Exercise 1 directory

--- Chapter 1_Exercise 1/Chapter_1_Exercise_1.py
+++
@@ -1,4 +0,0 @@
-#Exercise 1
-
-print('Python uses "interpreter" to convert source code into machine code') #It prints the message 1
-print("Interpreter 'translates' program one statement at a time") #It prints the message 2
