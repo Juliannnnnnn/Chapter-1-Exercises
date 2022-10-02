1422c6c36db46e7478aa9b700f91632663f9676d
Juliannnnnnn <91421563+Juliannnnnnn@users.noreply.github.com>
2022-10-02 14:49:28 +0400

    Add files via upload

---
+++ Chapter_1_Exercise_1.py
@@ -0,0 +1,4 @@
+#Exercise 1
+
+print('Python uses "interpreter" to convert source code into machine code') #It prints the message 1
+print("Interpreter 'translates' program one statement at a time") #It prints the message 2
